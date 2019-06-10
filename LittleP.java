eff05255bf1a91c774e03ffaf506d468340a4a9a
YuJia <yujiatheelder@gmail.com>
2019-06-10 22:37:46 +0800

    commit

--- LittleP.java
+++ LittleP.java
@@ -1,6 +1,6 @@
 public class LittleP {
     public static void main(String[] args) {
-        Lexer.readFile("a.p");
+        Lexer.readFile(args[0]);
         Tokenlist tl = new Tokenlist();
         Parsing p = new Parsing();
     }
