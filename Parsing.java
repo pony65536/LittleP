4aae77ceff22911810381429e13fc505198d3e48
YuJia <yujiatheelder@gmail.com>
2019-06-12 22:17:38 +0800

    commit

--- Parsing.java
+++ Parsing.java
@@ -5,9 +5,10 @@ public class Parsing {
     Tokenlist tl = new Tokenlist();
     private int depth;
 
-    private void printBranch() {
+    private void printBranch(String str) {
         for (int i = 0; i < depth; i++) {
             System.out.print("  ");
+            System.out.println(str);
         }
     }
 
@@ -18,23 +19,19 @@ public class Parsing {
 
     public void Prog() {            //程序
         depth++;
-        printBranch();
-        System.out.println("程序首部");
+        printBranch("程序首部");
         PH();
-        printBranch();
-        System.out.println("程序体");
+        printBranch("程序体");
         PB();
         depth--;
     }
 
     public void PH() {              //程序首部
         depth++;
-        printBranch();
-        System.out.println("program");
+        printBranch("program");
         if (tl.get(tokenLoc).getTag() == Tag.PROGRAM) {
             tokenLoc++;
-            printBranch();
-            System.out.println("程序名");
+            printBranch("程序名");
             PN();
         } else {
             System.out.println("Invalid Input");
@@ -45,30 +42,25 @@ public class Parsing {
 
     public void PB() {              //程序体
         depth++;
-        printBranch();
-        System.out.println("变量声明");
+        printBranch("变量声明");
         VD();
-        printBranch();
-        System.out.println("复合语句");
+        printBranch("复合语句");
         CS();
         depth--;
     }
 
     public void PN() {              //程序名
         depth++;
-        printBranch();
-        System.out.println("标识符");
+        printBranch("标识符");
         ID();
         depth--;
     }
 
     public void VD() {              //变量声明
         depth++;
-        printBranch();
-        System.out.println("var");
+        printBranch("var");
         if (tl.get(tokenLoc).getTag() == Tag.VAR) {
-            printBranch();
-            System.out.println("变量定义列表");
+            printBranch("变量定义列表");
             tokenLoc++;
             VSL();
         }
@@ -77,15 +69,12 @@ public class Parsing {
 
     public void VS() {              //变量定义
         depth++;
-        printBranch();
-        System.out.println("变量名列表");
+        printBranch("变量名列表");
         VNL();
         if (tl.get(tokenLoc).toString().equals(":")) {
-            printBranch();
-            System.out.println(":");
+            printBranch(":");
             tokenLoc++;
-            printBranch();
-            System.out.println("类型");
+            printBranch("类型");
             type();
         } else {
             System.out.println("Invalid Input");
@@ -97,15 +86,12 @@ public class Parsing {
     public void CS() {              //复合语句
         depth++;
         if (tl.get(tokenLoc).getTag() == Tag.BEGIN) {
-            printBranch();
-            System.out.println("begin");
+            printBranch("begin");
             tokenLoc++;
-            printBranch();
-            System.out.println("语句块");
+            printBranch("语句块");
             SB();
             if (tl.get(tokenLoc).getTag() == Tag.END) {
-                printBranch();
-                System.out.println("end");
+                printBranch("end");
             }
             tokenLoc++;
         } else {
@@ -118,24 +104,20 @@ public class Parsing {
     public void VSL() {             //变量定义列表
         depth++;
         while (!tl.get(tokenLoc).toString().equals(";")) {
-            printBranch();
-            System.out.println("变量定义");
+            printBranch("变量定义");
             VS();
         }
-        printBranch();
-        System.out.println(";");
+        printBranch(";");
         tokenLoc++;
         depth--;
     }
 
     public void VNL() {             //变量名列表
         depth++;
-        printBranch();
-        System.out.println("变量名");
+        printBranch("变量名");
         VN();
         if (tl.get(tokenLoc).toString().equals(",")) {
-            printBranch();
-            System.out.println(",");
+            printBranch(",");
             tokenLoc++;
             VNL();
         }
@@ -148,28 +130,24 @@ public class Parsing {
             System.out.println("Invalid Input");
             System.exit(0);
         }
-        printBranch();
-        System.out.println("integer");
+        printBranch("integer");
         tokenLoc++;
         depth--;
     }
     
     public void VN() {              //变量名
         depth++;
-        printBranch();
-        System.out.println("标识符");
+        printBranch("标识符");
         ID();
         depth--;
     }
 
     public void SB() {              //语句块
         depth++;
-        printBranch();
-        System.out.println("语句");
+        printBranch("语句");
         statement();
         if (tl.get(tokenLoc).toString().equals(";")) {
-            printBranch();
-            System.out.println(";");
+            printBranch(";");
             tokenLoc++;
             SB();
         }
@@ -180,20 +158,16 @@ public class Parsing {
         depth++;
         String str = tl.get(tokenLoc).toString();
         if (str.equals("if")) {
-            printBranch();
-            System.out.println("条件语句");
+            printBranch("条件语句");
             JS();
         } else if (str.equals("while")) {
-            printBranch();
-            System.out.println("循环语句");
+            printBranch("循环语句");
             LS();
         } else if (str.equals("begin")) {
-            printBranch();
-            System.out.println("复合语句");
+            printBranch("复合语句");
             CS();
         } else if (tl.get(tokenLoc).getTag() == Tag.ID) {
-            printBranch();
-            System.out.println("赋值语句");
+            printBranch("赋值语句");
             AS();
         } else {
             System.out.println("Invalid Input");
@@ -204,15 +178,12 @@ public class Parsing {
 
     public void AS() {              //赋值语句
         depth++;
-        printBranch();
-        System.out.println("左部");
+        printBranch("左部");
         LP();
         if (tl.get(tokenLoc).getTag() == Tag.IS) {
-            printBranch();
-            System.out.println(":=");
+            printBranch(":=");
             tokenLoc++;
-            printBranch();
-            System.out.println("右部");
+            printBranch("右部");
             RP();
         } else {
             System.out.println("Invalid Input");
@@ -224,25 +195,19 @@ public class Parsing {
     public void JS() {              //条件语句
         depth++;
         if (tl.get(tokenLoc).getTag() == Tag.IF) {
-            printBranch();
-            System.out.println("if");
+            printBranch("if");
             tokenLoc++;
-            printBranch();
-            System.out.println("关系表达式");
+            printBranch("关系表达式");
             RS();
             if (tl.get(tokenLoc).getTag() == Tag.THEN) {
                 tokenLoc++;
-                printBranch();
-                System.out.println("then");
-                printBranch();
-                System.out.println("语句");
+                printBranch("then");
+                printBranch("语句");
                 statement();
                 if (tl.get(tokenLoc).getTag() == Tag.ELSE) {
-                    printBranch();
-                    System.out.println("else");
+                    printBranch("else");
                     tokenLoc++;
-                    printBranch();
-                    System.out.println("语句");
+                    printBranch("语句");
                     statement();
                 } else {
                     System.out.println("Invalid Input");
@@ -262,18 +227,14 @@ public class Parsing {
     public void LS() {              //循环语句
         depth++;
         if (tl.get(tokenLoc).getTag() == Tag.WHILE) {
-            printBranch();
-            System.out.println("while");
+            printBranch("while");
             tokenLoc++;
-            printBranch();
-            System.out.println("关系表达式");
+            printBranch("关系表达式");
             RS();
             if (tl.get(tokenLoc).getTag() == Tag.DO) {
-                printBranch();
-                System.out.println("do");
+                printBranch("do");
                 tokenLoc++;
-                printBranch();
-                System.out.println("语句");
+                printBranch("语句");
                 statement();
             } else {
                 System.out.println("Invalid Input");
@@ -288,24 +249,21 @@ public class Parsing {
 
     public void LP() {              //左部
         depth++;
-        printBranch();
-        System.out.println("变量名");
+        printBranch("变量名");
         VN();
         depth--;
     }
 
     public void RP() {              //右部
         depth++;
-        printBranch();
-        System.out.println("算术表达式");
+        printBranch("算术表达式");
         MS();
         depth--;
     }
 
     public void MS() {              //算术表达式
         depth++;
-        printBranch();
-        System.out.println("项");
+        printBranch("项");
         nape();
         MS1();
         depth--;
@@ -314,20 +272,16 @@ public class Parsing {
     public void MS1() {             //算术表达式(消除左递归)
         String str = tl.get(tokenLoc).toString();
         if (str.equals("+")) {
-            printBranch();
-            System.out.println("+");
+            printBranch("+");
             tokenLoc++;
-            printBranch();
-            System.out.println("项");
+            printBranch("项");
             nape();
             MS1();
         }
         if (str.equals("-")) {
-            printBranch();
-            System.out.println("-");
+            printBranch("-");
             tokenLoc++;
-            printBranch();
-            System.out.println("项");
+            printBranch("项");
             nape();
             MS1();
         }
@@ -335,22 +289,18 @@ public class Parsing {
 
     public void RS() {              //关系表达式
         depth++;
-        printBranch();
-        System.out.println("算术表达式");
+        printBranch("算术表达式");
         MS();
-        printBranch();
-        System.out.println("关系运算符");
+        printBranch("关系运算符");
         OP();
-        printBranch();
-        System.out.println("算术表达式");
+        printBranch("算术表达式");
         MS();
         depth--;
     }
 
     public void nape() {            //项
         depth++;
-        printBranch();
-        System.out.println("因子");
+        printBranch("因子");
         factor();
         nape1();
         depth--;
@@ -359,20 +309,16 @@ public class Parsing {
     public void nape1() {           //项(消除左递归)
         String str = tl.get(tokenLoc).toString();
         if (str.equals("*")) {
-            printBranch();
-            System.out.println("*");
+            printBranch("*");
             tokenLoc++;
-            printBranch();
-            System.out.println("项");
+            printBranch("项");
             factor();
             nape1();
         }
         if (str.equals("/")) {
-            printBranch();
-            System.out.println("/");
+            printBranch("/");
             tokenLoc++;
-            printBranch();
-            System.out.println("项");
+            printBranch("项");
             factor();
             nape1();
         }
@@ -381,8 +327,7 @@ public class Parsing {
     public void ID() {              //标识符
         depth++;
         if (tl.get(tokenLoc).getTag() == Tag.ID) {
-            printBranch();
-            System.out.println(tl.get(tokenLoc).toString());
+            printBranch(tl.get(tokenLoc).toString());
             tokenLoc++;
         } else {
             System.out.println("Invalid Input");
@@ -395,32 +340,25 @@ public class Parsing {
         depth++;
         switch (tl.get(tokenLoc).toString()) {
             case ">=":
-                printBranch();
-                System.out.println(">=");
+                printBranch(">=");
                 break;
             case "<=":
-                printBranch();
-                System.out.println("<=");
+                printBranch("<=");
                 break;
             case "<>":
-                printBranch();
-                System.out.println("<>");
+                printBranch("<>");
                 break;
             case ":=":
-                printBranch();
-                System.out.println(":=");
+                printBranch(":=");
                 break;
             case "<":
-                printBranch();
-                System.out.println("<");
+                printBranch("<");
                 break;
             case ">":
-                printBranch();
-                System.out.println(">");
+                printBranch(">");
                 break;
             case "=":
-                printBranch();
-                System.out.println("=");
+                printBranch("=");
                 break;
             default:
                 System.out.println("Invalid Input");
@@ -433,8 +371,7 @@ public class Parsing {
     public void number() {          //数字
         depth++;
         if (tl.get(tokenLoc).getTag() == Tag.NUM) {
-            printBranch();
-            System.out.println(tl.get(tokenLoc).toString());
+            printBranch(tl.get(tokenLoc).toString());
             tokenLoc++;
         } else {
             System.out.println("Invalid Input");
@@ -446,23 +383,18 @@ public class Parsing {
     public void factor() {          //因子
         depth++;
         if (tl.get(tokenLoc).getTag() == Tag.NUM) {
-            printBranch();
-            System.out.println("数字");
+            printBranch("数字");
             number();
         } else if (tl.get(tokenLoc).getTag() == Tag.ID) {
-            printBranch();
-            System.out.println("标识符");
+            printBranch("标识符");
             ID();
         } else if (tl.get(tokenLoc).toString().equals("(")) {
-            printBranch();
-            System.out.println("(");
+            printBranch("(");
             tokenLoc++;
-            printBranch();
-            System.out.println("算术表达式");
+            printBranch("算术表达式");
             MS();
             if (tl.get(tokenLoc).toString().equals(")")) {
-                printBranch();
-                System.out.println(")");
+                printBranch(")");
                 tokenLoc++;
             } else {
                 System.out.println("Invalid Input");
